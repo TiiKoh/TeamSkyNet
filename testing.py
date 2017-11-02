7a68ccda72f66c269e5f92990b1b327e2e4a4e15
TiiKoh <33313710+TiiKoh@users.noreply.github.com>
2017-11-02 19:35:57 +0200

    Revert "asd"
    This reverts commit 64ba537059c0d010d23357c5c80da3add7fc6c15.

--- testing.py
+++ testing.py
@@ -1,7 +1,3 @@
 print ("Hello")
-name = input("What is your name? ")
-
-if name == "Tiina" or name == "Natalie":
-    print ("Greetings, ", name, ", your name is super cool!")
-else:
-    print ("Greetings,", name, "!")
+name = input("What is your name?")
+print ("Greetings,", name, "!")
